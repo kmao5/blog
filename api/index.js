24523fcabb348a141e7cf6cded4c9f883f14ac9a
kmao5 <katherine@lightyearstrategies.com>
2024-08-19 15:42:57 -0400

    Frontend deployment not working, going back to localhost

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ const secret = process.env.SECRET_KEY; // for jwt (login)
 
 // allows server to be accessible by other origins (browser domains)
 // also saves cookie as credentials
-app.use(cors({credentials: true, origin:'https://blog-chi-pearl.vercel.app/'})); 
+app.use(cors({credentials: true, origin:'https://localhost:3000'})); 
 
 app.use(express.json()); // parse incoming requests with json payload
 app.use(cookieParser());
